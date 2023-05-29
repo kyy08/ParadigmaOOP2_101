80c28e2cec8b06d266634af11640fc5bf52ab296
kyy08 <kyyelzz1@gmail.com>
2023-05-29 19:06:04 +0700

    membuat method class seseorang - polimorpi

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -9,35 +9,3 @@ class seseorang {
 	// }
 };
 
-class joko :public seseorang {
-	public:
-	void pesan() {
-		cout << "Pesan dar joko" << endl;
-	}
-};
-
-class lia :public seseorang {
-	public:
-	void pesan() {
-		cout << "Pesan dari lia" << endl;
-	}
-
-};
-
-int main() {
-	seseorang* obyek;
-	joko a;
-	lia b;
-
-	obyek = &a;
-	obyek->pesan();
-	obyek = &b;
-	obyek->pesan();
-	//a. seseorang::pesan();
-
-	return 0;
-}
-
-
-	
-
